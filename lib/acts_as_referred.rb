df8fcffbdcf263abd6fafd46b224ace875d65a8b
Rene Paulokat <rene@so36.net>
2013-09-26 00:28:03 +0200

    instance-methods

--- lib/acts_as_referred.rb
+++ lib/acts_as_referred.rb
@@ -8,14 +8,44 @@ module ActsAsReferred
     def acts_as_referred(options = {})
       cattr_accessor :referrer_field
       self.referrer_field = (options[:referrer_field] || :referred_from).to_s
+      
+      before_create :fill_referrer_field
 
       include ActsAsReferred::InstanceMethods
     end
+    
   end
 
   module InstanceMethods
+    
+    def referrer_uri
+      has_referrer? ? URI.parse(self.send(self.referrer_field)) : nil
+    end
+
     def referrer_host
-      URI.parse(self.send(self.referrer_field)).host
+      has_referrer? ? URI.parse(self.send(self.referrer_field)).host : nil
+    end
+
+    def referrer_scheme
+      has_referrer? ? URI.parse(self.send(self.referrer_field)).scheme : nil
+    end
+
+    def referrer_query
+      has_referrer? ? URI.parse(self.send(self.referrer_field)).query : nil
+    end
+
+    def referrer_path
+      has_referrer? ? URI.parse(self.send(self.referrer_field)).path : nil
+    end
+
+    def has_referrer?
+      true if self.send(self.referrer_field)
+    end
+
+    private
+
+    def fill_referrer_field
+      self.send("#{self.referrer_field}=", "fooobar")
     end
   end
 
--- test/acts_as_referred_test.rb
+++ test/acts_as_referred_test.rb
@@ -14,12 +14,54 @@ class ActsAsReferredTest < ActiveSupport::TestCase
   end
 
   test "test_a_booking_referrer_host_should_be_nsa" do
-    assert_equal "www.nsa.gov", Booking.new(valid_params).referrer_host
+    booking = Booking.new(valid_booking_params)
+    assert_equal "www.nsa.gov", booking.referrer_host
   end
 
+  test "test_a_order_referrer_host_should_be_nsa" do
+    booking = Order.new(valid_order_params)
+    assert_equal "www.nsa.gov", booking.referrer_host
+  end
 
-  def valid_params
-    { referred_from: 'http://www.nsa.gov/about/leadership/bio_jones.shtml'}
+  test "test_a_order_referrer_scheme_should_be_http" do
+    booking = Order.new(valid_order_params)
+    assert_equal 'http', booking.referrer_scheme
+  end
+
+  test "test_a_order_referrer_path_should_be_about" do
+    order = Order.new(valid_order_params)
+    assert_equal '/about/values/index.shtml', order.referrer_path
+  end
+
+  test 'test_a_order_query' do
+    order = Order.new(valid_order_params)
+    assert_equal 'attr=terror&reason=politics', order.referrer_query
+  end
+
+  test 'test_a_order_with_no_referrer_should_return_nil' do
+    order = Order.new
+    assert_equal nil, order.send(Order.referrer_field)
+  end
+
+  test 'test_a_order_with_no_referrer_should_return_nil_on_path' do
+    order = Order.new
+    assert_equal nil, order.referrer_path
+  end
+  
+  def valid_booking_params
+    { referrer: 'http://www.nsa.gov/about/values/index.shtml?attr=terror&reason=politics' }
+  end
+
+  def valid_order_params
+    { referred_from: 'http://www.nsa.gov/about/values/index.shtml?attr=terror&reason=politics'}
+  end
+
+  def order_google_params
+    { referred_from: 'https://play.google.com/store/apps/details?&referrer=utm_source%3Dnewsletter%26utm_medium%3Dcpc%26utm_term%3Dnsa%252Bsucks%26utm_campaign%3Dterrorcampaign' }
+  end
+
+  def piwik_campaign_params
+
   end
 end
 
