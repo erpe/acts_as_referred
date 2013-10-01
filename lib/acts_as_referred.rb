c8154e045e169d31e7c0261276be7da9956fbbc9
Rene Paulokat <rene@so36.net>
2013-10-01 23:10:17 +0200

    cleanup

--- lib/acts_as_referred.rb
+++ lib/acts_as_referred.rb
@@ -1,5 +1,4 @@
 require 'acts_as_referred/instance_methods'
-require 'acts_as_referred/rake_tasks'
 require 'acts_as_referred/model'
 require 'acts_as_referred/class_methods'
 require 'acts_as_referred/controller'
@@ -8,6 +7,13 @@ module ActsAsReferred
   extend ActiveSupport::Concern
   
   included do
+  end
+
+  # provide rake-tasks to create db-migration
+  class RakeTasks < Rails::Railtie
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+    end
   end
 
 end
--- lib/acts_as_referred/rake_tasks.rb
+++
@@ -1,7 +0,0 @@
-module ActsAsReferred
-  class RakeTasks < Rails::Railtie
-    rake_tasks do
-      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
-    end
-  end
-end
