ba74952831c6e97a1f0dbcfccb5835fa0fbec981
Rene Paulokat <rene@so36.net>
2013-10-02 14:35:34 +0200

    check if function is available before firing it

--- lib/acts_as_referred/instance_methods.rb
+++ lib/acts_as_referred/instance_methods.rb
@@ -5,13 +5,19 @@ module ActsAsReferred
 
     # after create hook to create a corresponding +Referee+
     def create_referrer
-      if struct = _get_reqref
+      
+      # will not respond to _get_reqref unless 
+      # reqref injected in application-controller
+      #
+      if self.respond_to?(:_get_reqref)
+        if struct = _get_reqref
 
-        self.create_referee(
-                    origin: struct.referrer_url, 
-                    request: struct.request_url, 
-                    visits: struct.visit_count
-                    )
+          self.create_referee(
+                      origin: struct.referrer_url, 
+                      request: struct.request_url, 
+                      visits: struct.visit_count
+                      )
+        end
       end
     end
 
